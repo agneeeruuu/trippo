ed24ebdf709d7bbdb942904aacb6ae52f629547a
p5l2b <agneeeruuu@gmail.com>
2020-12-25 18:34:56 +0800

    signin and register

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,18 +2,27 @@ import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
+import { userRegisterReducer, userSignInReducer } from './reducers/userReducer';
 
 const initialState = {
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo')
+            ? JSON.parse(localStorage.getItem('userInfo'))
+            : null,
+    },
     cart: {
         cartItems: localStorage.getItem('cartItems')
             ? JSON.parse(localStorage.getItem('cartItems'))
             : [],
     },
 };
+
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userSignin: userSignInReducer,
+    userRegister: userRegisterReducer
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
