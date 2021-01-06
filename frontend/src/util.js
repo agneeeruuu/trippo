757d8c2d8e355f98f238d5edd7295f94b3532cce
p5l2b <agneeeruuu@gmail.com>
2021-01-06 21:00:15 +0800

    search and filter

---
+++ frontend/src/util.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        name: 'Any', min: 0, max: 0
+    },
+    {
+        name: '$1 to $100', min: 1, max: 100
+    },
+    {
+        name: '$100 to $200', min: 100, max: 200
+    },
+    {
+        name: '$200 to $500', min: 200, max: 500
+    },
+    {
+        name: '$500 to $1000', min: 500, max: 1000
+    },
+];
+
+export const ratings = [
+    {
+        name: '4 stars +', rating: 4
+    },
+    {
+        name: '3 stars +', rating: 3
+    },
+    {
+        name: '2 stars +', rating: 2
+    },
+    {
+        name: '1 star +', rating: 1
+    },
+];
